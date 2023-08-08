a7d5afbc20c6424b341abfc96f71f8ca20785931
josneto <josneto@users.noreply.github.com>
2023-08-09 00:05:03 +0200

    Update settings.py

--- Force_Max/settings.py
+++ Force_Max/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['force-maximale.vercel.app', '127.0.0.1']
+ALLOWED_HOSTS = ['force-maximale-qxqc1p1kc-josneto.vercel.app', '127.0.0.1']
 
 
 # Application definition
