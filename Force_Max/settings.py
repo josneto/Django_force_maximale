eecff022e7e6f5b75ce7eda67a78c99c1bf6dd22
josneto <62713864+josneto@users.noreply.github.com>
2023-08-09 01:53:23 +0200

    Update settings.py

--- Force_Max/settings.py
+++ Force_Max/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = 'django-insecure-g*w1t9h-@z$hu22c!p&2)c4c^hn%&$pan83+s@@zd@p-b(al%n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
+ALLOWED_HOSTS = ['force-maximale.vercel.app', '127.0.0.1']
 
 
 # Application definition
